7ac3b019ba920ff095b29749e1c9b65279fc4a5e
jason <342690199@qq.com>
2018-11-28 19:36:32 +0800

    update model

--- Project1/first_react/src/models/example.js
+++ Project1/first_react/src/models/example.js
@@ -1,21 +1,25 @@
 
 export default {
-
+  // 命名空间，给模块取名字，想访问里面的数据先访问命名空间
   namespace: 'example',
 
+  // 存放的数据，做数据的初始化
   state: {},
 
+  // 监听功能，常用于路由的监听
   subscriptions: {
     setup({ dispatch, history }) {  // eslint-disable-line
     },
   },
 
+  // 副作用，异步操作 redux-saga
   effects: {
     *fetch({ payload }, { call, put }) {  // eslint-disable-line
       yield put({ type: 'save' });
     },
   },
 
+  // 常规的reducer，处理数据
   reducers: {
     save(state, action) {
       return { ...state, ...action.payload };
