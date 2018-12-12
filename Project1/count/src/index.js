44cd8bc5146eb976ae48127caa3031a821a2e940
jason <342690199@qq.com>
2018-12-12 23:14:06 +0800

    change index.js

--- Project1/count/src/index.js
+++ Project1/count/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// 获取store
+// get store
 import { Provider } from 'mobx-react';
 import stores from './store';
 
