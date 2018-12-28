5142c3c386189d9a3bdc5aaf6e570782ef74cd4e
jason <342690199@qq.com>
2018-12-28 08:56:16 +0800

    update vuex

--- Project2/count/src/App.vue
+++ Project2/count/src/App.vue
@@ -2,20 +2,29 @@
   <div>
     <button @click="changeNum('+')">+</button>
     <span>{{this.$store.state.app.num}}</span>
+    <span>千分符分割之后的数据： {{this.$store.getters['app/formatNum']}}</span>
     <button @click="changeNum('-')">-</button>
   </div>
 </template>
 <script>
 export default {
   mounted() {
-    console.log('this.$store', this.$store);
+    console.log('this.$store', this.$store, this.$store.getters)
   },
   methods: {
     changeNum(type){
       // this.$store.commit('changeNum', {payload: type});
-      this.$store.commit({
-        type: 'app/changeNum',
+      // 提交一个mutation
+      // this.$store.commit({
+      //   type: 'app/changeNum',
+      //   payload: type
+      // })
+      // 提交一个commit
+      this.$store.dispatch({
+        type: 'app/changeNumAsync',
         payload: type
+      }).then(res=>{
+        console.log('异步操作完成了!!!!');
       })
     }
   },
--- Project2/count/src/store/modules/app.js
+++ Project2/count/src/store/modules/app.js
@@ -3,16 +3,47 @@ const state = {
   num: 10000000
 }
 
+// 模块中派生的数据,可以缓存数据
+const getters = {
+  formatNum: (state)=>{
+    const re=/(?=(?!(\b))(\d{3})+$)/g;
+    let str = state.num.toString().replace(re,",");
+    return str;
+  }
+}
+
+
 // 模块中的同步改变
 const mutations = {
   changeNum(state, action){
     console.log('state...', state, 'action...', action);
-    state.num = action.payload=='+'?state.num+1: state.num-1;
+    // setTimeout(()=>{
+      state.num = action.payload=='+'?state.num+1: state.num-1;
+    // }, 100);
+  }
+}
+
+// 模块中的异步改变
+const actions = {
+  changeNumAsync({commit}, action){
+    return new Promise((resolve, reject)=>{
+       // console.log('context...', context, action);
+      setTimeout(()=>{
+        commit({
+          type: 'changeNum',
+          payload: action.payload
+        });
+        resolve();
+      }, 500);
+    })
   }
 }
 
 export default {
+  // 命名空间，做模块化
   namespaced: true,
   state,
+  getters,
+  actions,
   mutations
 }
